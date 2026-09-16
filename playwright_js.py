f9cdda035a81785059816b50801a5de535988b01
SergeyShein <sergeyshein20@gmail.com>
2026-09-16 19:43:21 +0200

    Углубление в Playwright

---
+++ playwright_js.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto(
+        'https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login',
+        wait_until='networkidle' #подождет, пока все запросы к серверу прогрузятся
+    )
+
+    page.evaluate(
+        """
+        const title = document.getElementById('authentication-ui-course-title-text')
+        title.textContent = 'New Text'
+        """
+    )
+
+    page.wait_for_timeout(3000)
