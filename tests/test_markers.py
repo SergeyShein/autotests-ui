1032227fe6ea1f9ce4891ff4eeaecdf617bb57b4
SergeyShein <sergeyshein20@gmail.com>
2026-09-22 13:48:50 +0200

    Маркировки Pytest

--- tests/test_markers.py
+++ tests/test_markers.py
@@ -4,3 +4,16 @@ import pytest
 @pytest.mark.smoke
 def test_some_case():
     ...
+
+
+@pytest.mark.regression
+def test_regression_case():
+    ...
+
+@pytest.mark.smoke
+class TestSuite:
+    def test_case(self):
+        ...
+
+    def test_case2(self):
+        ...
